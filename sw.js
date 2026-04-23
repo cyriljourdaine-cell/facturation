ce34ee26b31b0ea10f6fcd6352674bf752419a64
cyriljourdaine-cell <cyril.jourdaine@jourandlight.fr>
2026-04-23 19:44:48 +0200

    1.7.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '1.7.0';
+const VERSION = '1.7.2';
 const CACHE = 'facturation-' + VERSION;
 const ASSETS = ['./', './index.html', './manifest.json'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())); });
